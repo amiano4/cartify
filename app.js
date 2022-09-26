39b8e022e39cff47456ce76a981871bd5fa544bb
Almario Miano <107380357+amiano4@users.noreply.github.com>
2022-09-27 02:51:57 +0800

    added product view
    created dynamic modal for the product view

--- app.js
+++ app.js
@@ -30,33 +30,40 @@ function displayProducts(category = "") {
         if(json.length > 1) {
             json.forEach(product => {
                 // console.log(product.category)
-                const item = document.createElement('div');
+                const item = document.createElement('a');
+                item.href = product.title;
                 item.setAttribute('class', 'item-wrap');
                 item.innerHTML = `
-                <div class="item">
-                    <div class="img">
-                        <img src="${product.image}" alt="">
+                    <div class="item">
+                        <div class="img">
+                            <img src="${product.image}" alt="">
+                        </div>
+                        <div class="label">
+                            <span class="name">${product.title}</span>
+                            <span class="price">${product.price}</span>
+                            </div>
+                            <div class="p-actions">
+                                <button type="button" class="pa-btn place-order">
+                                    Checkout
+                                </button>
+                                <button type="button" class="pa-btn add-to-cart">
+                                    <i class="ri-shopping-cart-line"></i>
+                                </button>
+                            </div>
                     </div>
-                    <div class="label">
-                        <span class="name">${product.title}</span>
-                        <span class="price">${product.price}</span>
-                        </div>
-                        <div class="p-actions">
-                            <button type="button" class="pa-btn place-order">
-                                Checkout
-                            </button>
-                            <button type="button" class="pa-btn add-to-cart">
-                                <i class="ri-shopping-cart-line"></i>
-                            </button>
-                        </div>
-                </div>
                 `;
+
+                item.addEventListener('click', function(e) {
+                    e.preventDefault();
+
+                    openViewProductModal(product);
+                })
         
                 productsList.appendChild(item)
             });
         }
         else {
-            const item = document.createElement('div');
+            const item = document.createElement('a');
             item.setAttribute('class', 'item-wrap');
             item.innerHTML = `
             <div class="item">
@@ -81,4 +88,55 @@ function displayProducts(category = "") {
             productsList.appendChild(item)
         }
     })
+}
+
+function openViewProductModal(product) {
+    const modal = document.createElement('div');
+    modal.setAttribute('class', 'modal');
+    modal.innerHTML = `
+    <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title">View Product</h1>
+            <button type="button" class="modal-close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="p-grid">
+                <div class="p-view">
+                    <img src="shopping.jpg" alt="img">
+                </div>
+                <div class="p-details">
+                    <span class="name">${product.title}</span>
+                    <span class="description">${product.description}</span>
+                    <span class="price">Price per item: <span class="p">${product.price} USD</span></span>
+                </div>
+            </div>
+            <div class="checkout-details">
+                <div class="quantity">
+                    <div>Quantity</div>
+                    <button type="button"><</button>
+                    <input type="number" value="4">
+                    <button type="button">></button>
+                </div>
+                <div class="total">
+                    <div>Total amount: 12 USD</div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <div class="p-actions">
+                <a href="#" class="checkout">Checkout</a>
+                <a href="#" class="cart" style="margin-left: 20px;">
+                    Add to
+                    <i class="ri-shopping-cart-line"></i>   
+                </a>
+            </div>
+        </div>
+    </div>
+    `;
+
+    modal.querySelector('.modal-close').addEventListener('click', function() {
+        modal.remove();
+    })
+
+    document.body.append(modal);
 }
